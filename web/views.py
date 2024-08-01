4d54b2f8c62e2ab7f8281d74bb04b44a27523862
cis <shubham.pat@cisinlabs.com>
2024-08-01 18:05:34 +0530

    Initial Commit

---
+++ web/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homePage(request):
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def contact(request):
+    return render(request,"contact.html")
+
+def portfolio(request):
+    return render(request,"portfolio.html")
+
+def do(request):
+    return render(request,"do.html")
